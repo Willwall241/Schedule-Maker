370503dc43b3997276a1df3228434b470c40af6c
WJW <will.j.waller@gmail.com>
2018-10-16 19:06:52 -0400

    first commit

---
+++ client/build/precache-manifest.ce4261918552204585158c13eded9e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27c9b535a96d81e8a06f",
+    "url": "/static/js/main.27c9b535.chunk.js"
+  },
+  {
+    "revision": "1e29bc21ecfe86bb837a",
+    "url": "/static/js/1.1e29bc21.chunk.js"
+  },
+  {
+    "revision": "27c9b535a96d81e8a06f",
+    "url": "/static/css/main.db7956b0.chunk.css"
+  },
+  {
+    "revision": "d7794e0627fe6f91efc055d493e3f5f3",
+    "url": "/index.html"
+  }
+];
