70a91912db102b301fde3a637274ef8b3e7d2f9a
WJW <will.j.waller@gmail.com>
2018-10-19 23:48:46 -0400

    Update build

---
+++ client/build/precache-manifest.52d5d12440605ab56846bf9cc514fff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b163c1b5de6e1f9c53c7",
+    "url": "/static/js/main.b163c1b5.chunk.js"
+  },
+  {
+    "revision": "a61740b5f80532f6534a",
+    "url": "/static/js/1.a61740b5.chunk.js"
+  },
+  {
+    "revision": "b163c1b5de6e1f9c53c7",
+    "url": "/static/css/main.1dd14dee.chunk.css"
+  },
+  {
+    "revision": "4364d54de3336e3fc8e5ea64afae5e01",
+    "url": "/index.html"
+  }
+];
